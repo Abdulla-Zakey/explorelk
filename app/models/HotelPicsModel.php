48a7a2c99d2e4ccfb1f723e1210b1be5b8c9cd85
jabir0331 <mnnjabir@gmail.com>
2025-03-22 17:16:50 +0530

    Modified Hotel, HotelPics and HotelRoomTypeModels to cater the needs of both Hotel owners and travelers functionalities

--- app/models/HotelPicsModel.php
+++ app/models/HotelPicsModel.php
@@ -7,12 +7,21 @@
         protected $table = 'hotel_pics';
 
         protected $allowedColumns = [
+            'hotel_pic_Id',
             'hotel_Id',
             'image_path'
         ];
 
         public function getImagesByHotelId($hotel_Id){
-            $result = $this->where(['hotel_Id' => $hotel_Id]);
+            $result = $this->where(
+                [   
+                    'hotel_Id' => $hotel_Id
+                ],
+                [
+                    'order_by' => 'hotel_pic_Id',
+                    'order_type' => 'ASC'
+                ]
+            );
             return $result;
         }
 
